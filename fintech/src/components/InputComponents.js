e77fdee1da96db152aeadc9d7a4fbc82dd1dfcff
유관우 <skymill2000@gmail.com>
2022-06-28 13:31:42 +0900

    event 바인딩과 useState Hook

--- fintech/src/components/InputComponents.js
+++ fintech/src/components/InputComponents.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 //#rafce
 
 const InputComponents = () => {
-  let contents = "초기 콘텐츠 입니다.";
+  //   let contents = "초기 콘텐츠 입니다.";
+  const [contents, setContents] = useState("초기 콘텐츠입니다.");
 
   const handleChange = (event) => {
     const { value } = event.target;
-    console.log(value);
+    setContents(value);
   };
 
   return (
