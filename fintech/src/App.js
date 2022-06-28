5b14988a2429f6de2df4d3f3c941bdd875676d59
유관우 <skymill2000@gmail.com>
2022-06-28 14:55:45 +0900

    리액트 라우터 돔 라이브러리 적용

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,18 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
-import InputComponents from "./components/InputComponents";
-import ListComponent from "./components/ListComponent";
-import TopBar from "./components/common/TopBar";
-
 function App() {
   return (
-    <div className="App">
-      <TopBar></TopBar>
-      <Welcome username="유관우"></Welcome>
-      <InputComponents></InputComponents>
-      <ListComponent></ListComponent>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome></Welcome>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
