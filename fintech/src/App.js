3783327fe35da4aed0e9678357cfe8ec27a2844e
유관우 <skymill2000@gmail.com>
2022-06-28 14:59:12 +0900

    listcomponent 라우터 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
+import ListComponent from "./components/ListComponent";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome></Welcome>}></Route>
+        <Route path="/list" element={<ListComponent></ListComponent>}></Route>
       </Routes>
     </BrowserRouter>
   );
