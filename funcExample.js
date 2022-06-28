9e509c1c1b07b79a3df92a0c629141b8d3dfbffb
유관우 <skymill2000@gmail.com>
2022-06-28 10:26:04 +0900

    나누기 곱하기 완성

--- funcExample.js
+++ funcExample.js
@@ -10,5 +10,15 @@ const minus = (p1, p2) => {
 
 // div, multi
 
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
+const mul = (p1, p2) => {
+  return p1 * p2;
+};
+
 console.log(plus(2, 4));
 console.log(minus(5, 6));
+console.log(div(2, 4));
+console.log(mul(5, 6));
