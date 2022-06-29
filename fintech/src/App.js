e269f1ebbec43384a005678da06c193e9b416c22
유관우 <skymill2000@gmail.com>
2022-06-29 13:10:39 +0900

    사용자 인증 페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,15 +3,13 @@ import Welcome from "./components/Welcome";
 import ListComponent from "./components/ListComponent";
 import AxiosTest from "./pages/AxiosTest";
 import NewsApiPage from "./pages/NewsApiPage";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome></Welcome>}></Route>
-        <Route path="/list" element={<ListComponent></ListComponent>}></Route>
-        <Route path="/axiosTest" element={<AxiosTest></AxiosTest>}></Route>
-        <Route path="/news" element={<NewsApiPage></NewsApiPage>}></Route>
+        <Route path="/" element={<IndexPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fintech/src/pages/IndexPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "../components/common/TopBar";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <TopBar title={"사용자 인증"}></TopBar>
+      <button>사용자 인증받기</button>
+    </div>
+  );
+};
+
+export default IndexPage;
