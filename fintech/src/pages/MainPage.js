94d15c0e9dd76dd30dd80685c1f56a82c00c7309
유관우 <skymill2000@gmail.com>
2022-06-29 15:23:21 +0900

    실습  계좌 목록 불러오기 요청 작성

--- fintech/src/pages/MainPage.js
+++ fintech/src/pages/MainPage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TopBar from "../components/common/TopBar";
+import MainCard from "../components/main/MainCard";
 
 const MainPage = () => {
+  useEffect(() => {
+    console.log("안녕");
+    getAcountList();
+  }, []);
+
+  const getAcountList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+    console.log(accessToken, userSeqNo);
+    //axios 로 /v2.0/user/me?user_seq_no=1100034736 요청 작성하기
+    //header Authorization 값 입력
+  };
+
   return (
     <div>
       <TopBar title={"계좌 목록"} />
+      <MainCard></MainCard>
     </div>
   );
 };
