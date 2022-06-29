aa98dc7b6f777314e0c34bec4645fd76e289ebfc
유관우 <skymill2000@gmail.com>
2022-06-29 13:35:47 +0900

    토큰 발급 요청 작성하기

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import axios from "axios";
 const AuthResult = () => {
-  const parsed = queryString.parse(useLocation().search);
-  console.log(parsed.code);
-  return <div></div>;
+  const parsedCode = queryString.parse(useLocation().search).code;
+  const handleClick = () => {
+    //axios 활용하여 Post 요청 작성하기
+    const data = {};
+
+    const sendData = queryString.stringify(data);
+
+    const option = {
+      method: "POST",
+      url: "",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: sendData,
+    };
+    axios(option).then((response) => {
+      console.log(response);
+    });
+  };
+  return (
+    <div>
+      <p>인증코드 : {parsedCode}</p>
+      <button onClick={handleClick}>Access Token 발급 받기</button>
+    </div>
+  );
 };
 
 export default AuthResult;
