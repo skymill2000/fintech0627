1d445da30b55c19b773d8f9472c155e166ce41fd
유관우 <skymill2000@gmail.com>
2022-06-29 13:20:13 +0900

    사용자 인증처리 페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -4,12 +4,14 @@ import ListComponent from "./components/ListComponent";
 import AxiosTest from "./pages/AxiosTest";
 import NewsApiPage from "./pages/NewsApiPage";
 import IndexPage from "./pages/IndexPage";
+import AuthResult from "./pages/AuthResult";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexPage />}></Route>
+        <Route path="/authResult" element={<AuthResult />}></Route>
       </Routes>
     </BrowserRouter>
   );
