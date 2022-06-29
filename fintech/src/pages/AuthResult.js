9cdfea6b80d0ae16a4883e5023107f2d35e89b11
유관우 <skymill2000@gmail.com>
2022-06-29 13:44:48 +0900

    데이터 부분 완성

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -6,9 +6,17 @@ const AuthResult = () => {
   const parsedCode = queryString.parse(useLocation().search).code;
   const handleClick = () => {
     //axios 활용하여 Post 요청 작성하기
-    const data = {};
+    const data = {
+      code: parsedCode,
+      client_id: "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+      client_secret: "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+      redirect_uri: "http://localhost:3000/authResult",
+      grant_type: "authorization_code",
+    };
 
+    console.log(data);
     const sendData = queryString.stringify(data);
+    console.log(sendData);
 
     const option = {
       method: "POST",
