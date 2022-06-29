b161f375d537d5beb185a00b43c53d96c46c3d5b
유관우 <skymill2000@gmail.com>
2022-06-29 13:27:10 +0900

    쿼리 스트링 파싱 완료

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 const AuthResult = () => {
-  return <div>AuthResult</div>;
+  const parsed = queryString.parse(useLocation().search);
+  console.log(parsed.code);
+  return <div></div>;
 };
 
 export default AuthResult;
