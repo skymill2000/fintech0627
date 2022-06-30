5ef6da1b045a923df56870db0f37ab94ad7fa0f3
유관우 <skymill2000@gmail.com>
2022-06-30 13:16:35 +0900

    QR 코드 생성기

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import TopBar from "../components/common/TopBar";
+import { QRCodeSVG } from "qrcode.react";
+import styled from "styled-components";
+
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 
 const QrCodePage = () => {
   return (
     <div>
       <TopBar title={"QR코드"}></TopBar>
+      <QRBlock>
+        <QRCodeSVG size={200} value="https://reactjs.org/" />,
+      </QRBlock>
     </div>
   );
 };
