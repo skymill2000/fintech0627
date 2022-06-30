75cd0ee85c698386870d797e9216997fdd3c01c3
유관우 <skymill2000@gmail.com>
2022-06-30 10:36:04 +0900

    잔액조회 힌트2

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BalanceCard from "../components/balance/BalanceCard";
 import TopBar from "../components/common/TopBar";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import axios from "axios";
 
 const BalancePage = () => {
+  const fintechUseNo = queryString.parse(useLocation().search).fintechUseNo;
+  console.log(fintechUseNo);
+  useEffect(() => {
+    getBalance();
+  }, []);
+
+  const getBalance = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+    console.log(accessToken, userSeqNo);
+    const option = {
+      method: "GET",
+      url: "",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: { user_seq_no: userSeqNo },
+    };
+    axios(option).then(({ data }) => {});
+  };
+  //useEffect
+  //amount state
+  //axios 요청을 작성
+  //banktransid 작성 ??<----
   return (
     <div>
       <TopBar title={"잔액조회"}></TopBar>
