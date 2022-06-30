c063e685bd5a73046f1ef852a4434b632d6bbc4e
유관우 <skymill2000@gmail.com>
2022-06-30 15:03:21 +0900

    출금 입금 이체 필요 컴퍼넌트 추가

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "../components/common/TopBar";
+import { QrReader } from "react-qr-reader";
 
 const QrReaderPage = () => {
+  const [data, setData] = useState("No result");
+
   return (
     <div>
       <TopBar title={"QR 리더"}></TopBar>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
     </div>
   );
 };
