35b5b8cfc398da148eed0fbea7fe5aa9dbee65ea
유관우 <skymill2000@gmail.com>
2022-06-30 15:10:04 +0900

    결제 화면 팝업 모달 완성

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
 import TopBar from "../components/common/TopBar";
 import { QrReader } from "react-qr-reader";
+import ModalWithdraw from "../components/withdraw/ModalWithdraw";
+import Modal from "react-modal";
 
 const QrReaderPage = () => {
   const [data, setData] = useState("No result");
+  const [openModal, setOpenModal] = useState(false); //no camera : true
+  const CustomStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      zIndex: "9",
+    },
+    content: {
+      width: "95%",
+      border: `0 solid black`,
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      zIndex: "99999",
+    },
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
 
   return (
     <div>
@@ -12,6 +36,7 @@ const QrReaderPage = () => {
         onResult={(result, error) => {
           if (!!result) {
             setData(result?.text);
+            setOpenModal(true);
           }
           if (!!error) {
             console.info(error);
@@ -20,6 +45,13 @@ const QrReaderPage = () => {
         style={{ width: "100%" }}
       />
       <p>{data}</p>
+      <Modal
+        isOpen={openModal}
+        style={CustomStyles}
+        onRequestClose={closeModal}
+      >
+        <ModalWithdraw tofintechno={data}></ModalWithdraw>
+      </Modal>
     </div>
   );
 };
