8b3391cd659fd212181625fcd565801d137878de
유관우 <skymill2000@gmail.com>
2022-07-01 10:48:29 +0900

    쿼리 스트링을통한 응답 작성 /

--- server/expressServer.js
+++ server/expressServer.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("안녕하세요 익스프레스 서버 입니다.");
+});
+
+app.get("/getParams", (req, res) => {
+  console.log(req.query);
+  res.send(`너의 핀테크 번호는 ${req.query.fintech_use_num}`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
